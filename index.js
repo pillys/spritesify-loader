976d4b846da5522581f06e8792a1835d4e8af2d1
Pillys <pillys@163.com>
2016-05-23 17:27:03 +0800

    ++

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 'use strict';
-var sprites = require('spritesify');
+var Spritesify = require('spritesify');
 var loaderUtils = require('loader-utils');
 
 module.exports = function(source) {
   var callback = this.async();
   var query = loaderUtils.parseQuery(this.query);
-  sprites(query.spritesPath, false, query.urlFix, function(err, data) {
-    if (err) {
-      return callback(err);
+  var spritesify = new Spritesify(query.spritesPath, query.urlFix);
+  spritesify.css(query.distPath, function(err, data) {
+    if(err) {
+      callback(err);
     } else {
       callback(null, data + source);
     }
