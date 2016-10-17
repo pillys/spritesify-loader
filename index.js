eb1d35722f015945f421047ae3a187c7fa834034
Pillys <pillys@163.com>
2016-10-17 15:22:13 +0800

    2.3.0

--- index.js
+++ index.js
@@ -1,16 +1,23 @@
 'use strict';
+var fs = require('fs');
 var Spritesify = require('spritesify');
 var loaderUtils = require('loader-utils');
 
 module.exports = function(source) {
   var callback = this.async();
   var query = loaderUtils.parseQuery(this.query);
-  var spritesify = new Spritesify(query.spritesPath, query.urlFix);
-  spritesify.css(query.distPath, function(err, data) {
-    if(err) {
-      callback(err);
+  fs.exists(query.spritesPath, function(exists) {
+    if (exists) {
+      var spritesify = new Spritesify(query.spritesPath, query.urlFix);
+      spritesify.css(query.distPath, function(err, data) {
+        if (err) {
+          callback(err);
+        } else {
+          callback(null, data + source);
+        }
+      });
     } else {
-      callback(null, data + source);
+      callback(null, source);
     }
   });
 };
